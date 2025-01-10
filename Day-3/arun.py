2c8913b7b429f626aacd960c1228fb2ea7a888a5
arun <arunkharwar2003@gmail.com>
2025-01-10 20:04:51 +0530

    nesting

---
+++ Day-3/arun.py
@@ -0,0 +1,57 @@
+                 # PRACTICE #
+
+# write a program to input 2 numbers and print their sum.
+
+"""first = int( input("enter first number : "))
+second = int (input("enter second number : "))
+
+print( "user given output : " , first + second)
+"""
+
+# WAP to input 2 int numbers, a and b
+# print true if a is greater than or equal to b. If not print false
+""""
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+
+print(a >= b)
+
+# print the length of the strings
+a= "arun"
+len1 = len(a)
+print(len1)
+c = "kharwar"
+len3=len(c)
+print(len3)
+final_str = a + c
+print(final_str)
+print(len(final_str))
+
+
+# write a program to input users first name and print its length
+
+name = input("enter your name : ")
+len = len(name)
+print(len)
+
+
+
+# write a program to fined the occurrence of '$' in a string.
+str = "Hi, $Iam the $ sysmbol $99.99"
+print(str.count("$"))
+
+"""
+
+                             # Nesting #
+
+age = 95
+
+if (age >= 18):
+    if(age >= 80):
+        print("cannot drive")
+    else:
+        print("can drive")
+else:
+    print("cannot drive")            
+
+
