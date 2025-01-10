40c7762a9e8b6d74f4a1727a77967022096a4998
arun <arunkharwar2003@gmail.com>
2025-01-10 17:23:31 +0530

    conditions works successfully

--- Day-3/arun.py
+++
@@ -1,7 +0,0 @@
-# print output for :
-
-# A = 5 and G = M
-# A = 2 and G = F
-
-A = int(input ("enter you number : "))
-G = input("M/F : ")
--- script.py
+++ script.py
@@ -4,22 +4,22 @@ import time
 def git_auto_update():
     while True:
         try:
-            # Stage all changes
+           
             subprocess.run(["git", "add", "."])
             
-            # Commit changes with a default message
-            commit_message = "conditions works successfully"
+            
+            commit_message = "conditions works successfully"             # Commit changes with a default message
             subprocess.run(["git", "commit", "-m", commit_message])
             
-            # Push changes to the main branch
+            
             subprocess.run(["git", "push", "-u", "origin", "main"])
             
             print("Changes pushed successfully!")
         except subprocess.CalledProcessError as e:
             print(f"An error occurred: {e}")
         
-        # Wait for 5 minutes (300 seconds) before the next update
-        time.sleep(300)
+       
+        time.sleep(300)    # Wait for 5 minutes (300 seconds) before the next update
 
 # Run the auto-update function
 git_auto_update()
