720d7db7912533275f1bce26384bde7ad6db6884
arun <arunkharwar2003@gmail.com>
2025-01-10 14:43:33 +0530

    taking input values from user side

--- Day-2/arun.py
+++ Day-2/arun.py
@@ -1,6 +1,9 @@
 # input() statement is used to accept values (using keyboard) form user
 
 # string input
- name = input("name ")
+name = input("name :")
 
-#
+# int input
+
+age = int(input("age :"))
+
