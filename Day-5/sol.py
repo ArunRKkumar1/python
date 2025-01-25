18d1e1e36cc7698f6c79b6322e3a04ce6806c8ea
arun <arunkharwar2003@gmail.com>
2025-01-25 06:53:25 +0530

    solve some questions

--- Day-5/sol.py
+++ Day-5/sol.py
@@ -10,7 +10,7 @@ b = c
 print("value of a : ", a)
 print("value of b : ", b)
 
-'''
+
 
 a = 30
 b = 40
@@ -18,3 +18,25 @@ b = 40
 a, b = b,a
 print("value of a : ", a)
 print("value of b : ", b)
+
+
+# WAP to convert a float into int
+
+a = 12.5
+print(type(a))
+
+a = int(a)
+print(type(a))
+print(a)
+
+'''
+
+# WAP to take details from a student for a id card
+
+name = input("enter a student name :")
+grade = input("ente a student grade :")
+age = input("enter a student age :")
+email = input("ente a student email id :")
+phone_no = input("ente a student phone number :")
+print(name, "\n", grade, "\n", age, "\n", email, "\n", phone_no)
+
